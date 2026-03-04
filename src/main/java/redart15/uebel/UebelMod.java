f394c715b15ac25a9356cab473740f7e0b92df06
Redart15 <redart15@users.noreply.github.com>
2026-03-04 23:09:20 +0100

    changes the mod name and adjusted it everywhere

--- src/main/java/redart15/uebel/UebelMod.java
+++ src/main/java/redart15/uebel/UebelMod.java
@@ -1,4 +1,4 @@
-package turniplabs.examplemod;
+package redart15.uebel;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
@@ -6,12 +6,12 @@ import org.slf4j.LoggerFactory;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
-public class ExampleMod implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
-	public static final String MOD_ID = "examplemod";
+public class UebelMod implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
+	public static final String MOD_ID = "uebel";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	@Override
 	public void onInitialize() {
-		LOGGER.info("ExampleMod initialized.");
+		LOGGER.info("Übel initialized.");
 	}
 
 	@Override
