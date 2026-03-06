a4fcd8e7b47da1d5ec0f77f5c6620de1f1c7e4e9
Redart15 <redart15@users.noreply.github.com>
2026-03-06 10:33:46 +0100

    load recipes

--- src/main/java/redart15/uebel/recipe/UebelRecipes.java
+++ src/main/java/redart15/uebel/recipe/UebelRecipes.java
@@ -1,15 +1,25 @@
 package redart15.uebel.recipe;
 
+import net.minecraft.core.data.DataLoader;
+import turniplabs.halplibe.helper.RecipeBuilder;
 import turniplabs.halplibe.util.RecipeEntrypoint;
+
+import static redart15.uebel.UebelMod.MOD_ID;
 
 public class UebelRecipes implements RecipeEntrypoint {
 	@Override
 	public void onRecipesReady() {
-
+		UebelRecipes.vanillaMachines();
 	}
 
 	@Override
 	public void initNamespaces() {
+		RecipeBuilder.initNameSpace(MOD_ID);
+	}
 
+	public static void vanillaMachines() {
+		DataLoader.loadRecipesFromFile("/assets/" + MOD_ID + "/recipes/workbench.json");
+		DataLoader.loadRecipesFromFile("/assets/" + MOD_ID + "/recipes/furnace.json");
+		DataLoader.loadRecipesFromFile("/assets/" + MOD_ID + "/recipes/blast_furnace.json");
 	}
 }
